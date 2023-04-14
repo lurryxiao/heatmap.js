7b7881a1dcf60d5aba026ef1c575e0d75965b9e8
word project <2624606482@qq.com>
2023-04-14 19:46:21 +0800

    Update heatmap.js

--- build/heatmap.js
+++ build/heatmap.js
@@ -524,8 +524,8 @@ var Canvas2dRenderer = (function Canvas2dRendererClosure() {
 
       }
 
-      img.data = imgData;
-      this.ctx.putImageData(img, x, y);
+      var newimg = {...img, data: imgData};
+      this.ctx.putImageData(newimg, x, y);
 
       this._renderBoundaries = [1000, 1000, 0, 0];
 
